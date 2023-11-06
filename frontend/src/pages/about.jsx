3aca98d66ea0e7058216c83408d3a4a6e3b4ab6e
Mouayad Mouayad <mouayad1998@hotmail.com>
2023-11-06 12:16:33 +0100

    s

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import AboutPageHero from '../components/about/AboutPageHero';
 import PageMeta from '../components/meta/PageMeta';
 import FeatureImgThree from '../components/features/FeatureImgThree';
-import Team from '../components/team/team';
 
 import FooterOne from '../components/footer/FooterOne';
 import Navbar from '../components/header/Navbar';
 import Layout from '../components/layout/Layout';
-import { PopupWidget } from 'react-calendly';
-// import TeamMember from './teamMember';
 
 const About = () => {
   return (
@@ -21,8 +18,7 @@ const About = () => {
       <Navbar />
       <AboutPageHero />
       <FeatureImgThree />
-      {/* <Team />{" "} */}
-      <FooterOne footerGradient />{' '}
+      <FooterOne footerGradient />
     </Layout>
   );
 };
