bbeeb0f963f92c28ad2e7d8a0e2db5478ad1743f
Mouayad Mouayad <mouayad1998@hotmail.com>
2024-02-13 19:04:43 +0100

    Added @vercel/speed-insights/next

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -20,22 +20,11 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "aos/dist/aos.css"; // You can also use <link> in your document's <head> section
-import AOS from "aos";
 
 import type { AppProps } from "next/app";
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	React.useEffect(() => {
-		AOS.init({
-			// Settings that you can configure
-			offset: 200, // offset (in px) from the original trigger point
-			delay: 0, // values from 0 to 3000, with step 50ms
-			duration: 1000, // values from 0 to 3000, with step 50ms
-			once: false, // whether animation should happen only once - while scrolling down
-			mirror: false, // whether elements should animate out while scrolling past them
-		});
-	}, []);
 	return (
 		<>
 			<Analytics />
