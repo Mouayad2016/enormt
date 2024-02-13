40b9ae4b1c16093b3f8cce0fcbcfc3b4136e1999
Mouayad Mouayad <mouayad1998@hotmail.com>
2024-02-13 19:15:56 +0100

    Added @vercel/speed-insights/next

--- frontend/src/components/header/PageHeader.jsx
+++ frontend/src/components/header/PageHeader.jsx
@@ -4,14 +4,14 @@ const PageHeader = ({ title, desc }) => {
 	return (
 		<>
 			{
-				<section className=' position-relative overflow-hidden ptb-60'>
+				<section className='gradient-backgroud position-relative overflow-hidden ptb-60'>
 					<div className='container pt-5'>
 						<div className={`row`}>
 							<div className='col-lg-8 col-md-12'>
 								<h1 className='display-5 fw-bold text-dark pt-50 text-center-mobile'>
 									{title}
 								</h1>
-								<p className='text-dark res-fs-5 text-center-mobile pt-20 '>
+								<p className='text-dark fs-5 text-center-mobile pt-20 '>
 									{desc}
 								</p>
 							</div>
