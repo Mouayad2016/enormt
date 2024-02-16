9a3d0f58b421b9e60625db84869e0eb341399dde
Mouayad Mouayad <mouayad1998@hotmail.com>
2024-02-16 16:26:25 +0100

    Removed unused dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,25 @@
-const withSvgr = require('next-svgr');
+const withSvgr = require("next-svgr");
 
 const nextConfig = {
-  // Enables React strict mode for development
-  reactStrictMode: true,
+	// Enables React strict mode for development
+	reactStrictMode: true,
 
-  // Disables the "Powered by Vercel" header in the server response (for security reasons)
-  poweredByHeader: false,
+	// Disables the "Powered by Vercel" header in the server response (for security reasons)
+	poweredByHeader: false,
 
-  // Specifies custom paths for the `dist` directory (change this if you need a custom build directory)
-  // distDir: 'build',
-
-  // Add custom environmental variables here that you want to expose to the client side
-  // env: {
-  //   CUSTOM_ENV_VARIABLE: 'my-value',
-  // },
-
-  // Include any other configurations you might want
+	async headers() {
+		return [
+			{
+				source: "/assets/(.*)",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=604800, must-revalidate", // Set a max-age appropriate for your assets
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = withSvgr(nextConfig);
