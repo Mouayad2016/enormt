1f541721cae163060108b4febbdc8eec295c98eb
Mouayad Mouayad <mouayad1998@hotmail.com>
2024-02-16 15:35:05 +0100

    Removed unused dependencies

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -36,7 +36,7 @@ class MyDocument extends Document {
 
 					<meta
 						name='viewport'
-						content='width=device-width, initial-scale=1.0, maximum-scale=1.0'
+						content='width=device-width, initial-scale=1.0'
 					/>
 					<meta name='theme-color' content='#000000' />
 
