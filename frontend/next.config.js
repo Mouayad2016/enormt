e5076f78c5905d3ccc9f3262b17bc25aafa379f8
Mouayad Mouayad <mouayad1998@hotmail.com>
2024-02-20 18:50:07 +0100

    Fixing demo page performance

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,14 +10,27 @@ const nextConfig = {
 	async headers() {
 		return [
 			{
-				source: "/assets/(.*)",
+				// Applying cache control to all assets under the /assets directory
+				source: "/assets/:path*",
 				headers: [
 					{
 						key: "Cache-Control",
-						value: "public, max-age=604800, must-revalidate", // Set a max-age appropriate for your assets
+						// Setting cache control for immutable assets to 1 year
+						value: "public, max-age=31536000, immutable",
 					},
 				],
 			},
+			{
+				// Extend this pattern to other asset directories as needed, for example, for SVG assets handled by SVGR
+				source: "/images/:path*",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+				],
+			},
+			// Add more patterns here if you have other asset types or directories
 		];
 	},
 };
